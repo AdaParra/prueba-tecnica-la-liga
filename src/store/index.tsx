9547529ce697d0f0acf3ea88294a0b6281a44382
Paola Parra <adapparrad@gmail.com>
2022-01-16 14:57:31 +0000

    Updated dependencies

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,5 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
 
