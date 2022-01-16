4183811ca642f9aeb0ba242a72588135651b654a
Paola Parra <adapparrad@gmail.com>
2022-01-16 17:50:05 +0000

    Log in
    Using the user authentication request to login in the login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import authentication from '../api/index';
 
 export default function Login() {
+
+    const [email, setEmail] = useState('eve.holt@reqres.in');
+    const [password, setPassword] = useState('cityslicka');
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await authentication(email, password);
+        if(response?.ok){
+            const { token } = await response.json();
+            console.log(token);
+        }
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input autoComplete="off" type="text" name="email" onChange={() => {} /* e => setEmail(e.target.value) */} />
+            <input autoComplete="off" type="password" name="password" onChange={() => {} /*e => setPassword(e.target.value)*/} />
+            <button type="submit">
+                Iniciar Sesión
+            </button>
+        </form>
     )
 }
