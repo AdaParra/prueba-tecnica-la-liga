88df0473b96960bcff5614e6a30f5c621d1acbcc
Paola Parra <adapparrad@gmail.com>
2022-01-19 10:42:29 +0000

    Private route and route not found
    Added redirect in login page when the user is logged in.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
 import { loginWith } from '../api/index';
 
 export default function Login() {
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showMessageUnauthorized, setshowMessageUnauthorized] = useState(false);
     
     const handleSubmit = async (event) => {
         event.preventDefault();
-        await loginWith(email, password);
+        const HttpStatusCodes = await loginWith(email, password);
+        if (HttpStatusCodes === 200) {
+            navigate('/users');
+        } else {
+            setshowMessageUnauthorized(true);
+        }
+    }
+
+    if (localStorage.getItem('token')) {
+        return <Navigate to="/users" />
     }
 
     return (
@@ -17,6 +29,9 @@ export default function Login() {
             <button type="submit">
                 Iniciar Sesión
             </button>
+            {
+                showMessageUnauthorized && <p>Usuario o contraseña incorrecta</p>
+            }
         </form>
     )
 }
