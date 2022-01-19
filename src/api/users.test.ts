01ca86b6da1fbaef4db99390c8745070bcc50003
Paola Parra <adapparrad@gmail.com>
2022-01-19 20:51:09 +0000

    extracting method

--- src/api/users.test.ts
+++ src/api/users.test.ts
@@ -10,33 +10,7 @@ describe("users.ts", () => {
             total: 20,
             total_pages: 40
         }
-        global.fetch = jest.fn(() => Promise.resolve({
-            json: () => Promise.resolve(usersInfo),
-            headers: new Headers(),
-            ok: false,
-            redirected: false,
-            status: 0,
-            statusText: "",
-            type: "basic",
-            url: "",
-            clone: function (): Response {
-                throw new Error("Function not implemented.")
-            },
-            bodyUsed: false,
-            body: null,
-            arrayBuffer: function () {
-                throw new Error("Function not implemented.")
-            },
-            blob:  function () {
-                throw new Error("Function not implemented.")
-            },
-            formData:  function () {
-                throw new Error("Function not implemented.")
-            },
-            text:  function () {
-                throw new Error("Function not implemented.")
-            },
-        }))
+        global.fetch = mockFetchApiWith(usersInfo)
 
         const numberOfPages = 6
         getUsers(numberOfPages)
@@ -44,3 +18,33 @@ describe("users.ts", () => {
         expect(global.fetch).toHaveBeenCalled()
     })
 })
+
+function mockFetchApiWith(usersInfo: UsersInformation): (input: RequestInfo, init?: RequestInit | undefined) => Promise<Response> {
+    return jest.fn(() => Promise.resolve({
+        json: () => Promise.resolve(usersInfo),
+        headers: new Headers(),
+        ok: false,
+        redirected: false,
+        status: 0,
+        statusText: "",
+        type: "basic",
+        url: "",
+        clone: function (): Response {
+            throw new Error("Function not implemented.")
+        },
+        bodyUsed: false,
+        body: null,
+        arrayBuffer: function () {
+            throw new Error("Function not implemented.")
+        },
+        blob: function () {
+            throw new Error("Function not implemented.")
+        },
+        formData: function () {
+            throw new Error("Function not implemented.")
+        },
+        text: function () {
+            throw new Error("Function not implemented.")
+        },
+    }))
+}
