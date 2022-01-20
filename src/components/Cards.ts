b3fb91c70fa874b95ab40f27255eac48d761e2ac
Paola Parra <adapparrad@gmail.com>
2022-01-20 14:41:02 +0000

    responsive cards

--- src/components/Cards.ts
+++ src/components/Cards.ts
@@ -3,7 +3,7 @@ import { colors } from "./Theme";
 
 export const Cards = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-auto-rows: auto;
     grid-gap: 1rem;
 `;
