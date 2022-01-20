54029e7510db0af8a76765aa656bb1340e7b7f84
Paola Parra <adapparrad@gmail.com>
2022-01-20 22:41:42 +0000

    splitting tests

--- src/pages/Login.test.tsx
+++ src/pages/Login.test.tsx
@@ -5,23 +5,56 @@ import { createMemoryHistory } from 'history';
 import Login from './Login';
 
 describe('Login.tsx', () => {
-  test('shows the login page correctly', () => {
+  test('shows the title correctly', () => {
     const history = createMemoryHistory();
 
-    const { getByText, getByLabelText, getByRole } = render(
+    const { getByText } = render(
       <Router location={history.location} navigator={history}>
         <Login />
       </Router>,
     );
 
     const loginTitle = getByText(/Proyecto LaLiga/);
-    const emailInput = getByLabelText('email');
-    const passwordInput = getByLabelText('password');
-    const submitButton = getByRole('button');
     expect(loginTitle).toBeInTheDocument();
+  });
+
+  test('shows the email input correctly', () => {
+    const history = createMemoryHistory();
+
+    const { getByLabelText } = render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>,
+    );
+
+    const emailInput = getByLabelText('email');
     expect(emailInput).toHaveAttribute('placeholder', 'Email');
     expect(emailInput).toHaveAttribute('type', 'email');
+  });
+
+  test('shows the password input correctly', () => {
+    const history = createMemoryHistory();
+
+    const { getByLabelText } = render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>,
+    );
+
+    const passwordInput = getByLabelText('password');
     expect(passwordInput).toHaveAttribute('placeholder', 'Contraseña');
+  });
+
+  test('shows the submit button correctly', () => {
+    const history = createMemoryHistory();
+
+    const { getByRole } = render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>,
+    );
+
+    const submitButton = getByRole('button');
     expect(submitButton).toHaveTextContent('Iniciar Sesión');
   });
 });
